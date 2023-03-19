4912df728b8c5b2657257bb0cc8cc06b8d962cb1
GenIl <topgear09@yandex.ru>
2023-03-19 11:39:18 +0300

    Добавлен и настроен mock для запросов к серверу

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './app/App';
 import store from './app/store';
 import { Provider } from 'react-redux';
+import { worker } from './mocks/browser';
+
+if (process.env.NODE_ENV === 'development') {
+  worker.start()
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
